f5de7d4c5dd5a4af4c8387be85332361ead26d40
Venkat <venkatrao727@gmail.com>
2014-07-29 13:21:59 +0530

    adding users

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,51 +1,28 @@
 require 'spec_helper'
 
-describe "Static pages" do
-  describe "Home page" do
-    it "should have the content 'Sample App'" do
-	visit root_path
-	expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title 'Home'" do
-	visit root_path
-	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
+  describe "Static pages" do
+   subject { page }
+   describe "Home page" do
+     before { visit root_path }
+     it { should have_content('Sample App') }
+     it { should have_title(full_title(" Home")) }
   end
  
-  describe "Help page" do
-	it "should contain the content 'Help'" do
-	  visit help_path
-	  expect(page).to have_content('Help')
-	end
-
-  	it "should have the title 'Help'" do
-	  visit help_path
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+   describe "Help page" do
+     before{ visit help_path }
+     it{ should have_content('Help') }
+     it { should have_title("Ruby on Rails Tutorial Sample App | Help") }
   end
 
   describe "About Page" do
-	it "Should contain content 'About Us'" do
-	  visit about_path
-	  expect(page).to have_content('About Us')
-	end
-	
-	it "should have the title 'About'" do
-	  visit about_path
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
-    end
+     before{ visit about_path }
+     it { should have_content('About Us') }
+     it { should have_title("Ruby on Rails Tutorial Sample App | About")}
   end
 
  describe "Contact Page" do
- 	it "should have content as 'Contact'" do
-	  visit contact_path
-	  expect(page).to have_content('Contact')
-	end	
-	
-	it "should have tile as 'Contact'" do
-	  visit contact_path
-	  expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
-	end	
+    before { visit contact_path }
+    it { should have_content('Contact') }
+    it { should have_title('Ruby on Rails Tutorial Sample App | Contact') }
  end
 end
