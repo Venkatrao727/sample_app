38e66ffcea3cc953402fe6a684049dcfa7767fe9
Venkat <venkatrao727@gmail.com>
2014-07-29 10:58:44 +0530

    final commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,38 +3,49 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-	visit '/static_pages/home'
+	visit root_path
 	expect(page).to have_content('Sample App')
     end
 
     it "should have the title 'Home'" do
-	visit '/static_pages/home'
+	visit root_path
 	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
  
   describe "Help page" do
 	it "should contain the content 'Help'" do
-	  visit '/static_pages/help'
+	  visit help_path
 	  expect(page).to have_content('Help')
 	end
 
   	it "should have the title 'Help'" do
-	  visit '/static_pages/help'
+	  visit help_path
 	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About Page" do
 	it "Should contain content 'About Us'" do
-	  visit '/static_pages/about'
+	  visit about_path
 	  expect(page).to have_content('About Us')
 	end
 	
 	it "should have the title 'About'" do
-	  visit '/static_pages/about'
+	  visit about_path
 	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
     end
-
   end
+
+ describe "Contact Page" do
+ 	it "should have content as 'Contact'" do
+	  visit contact_path
+	  expect(page).to have_content('Contact')
+	end	
+	
+	it "should have tile as 'Contact'" do
+	  visit contact_path
+	  expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+	end	
+ end
 end
